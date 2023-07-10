8dc276d60e51223b190d41e55f61f0bd433fc1b7
salvomc <smcrwrk@gmail.com>
2023-07-10 16:31:00 +0200

    implented class genre and movie

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
 <?php
 
+    class Genre {
+        public $name;
+
+        public function __construct($name)
+        {
+            $this->name = $name;
+        }
+    }
+
+    class Movie {
+        public $title;
+        public $year;
+        public $genre;
+        public $length;
+        public $rating;
+        
+        public function __construct($title, $year, $genre, $lenght)
+        {
+            $this->title = $title;
+            $this->year = $year;
+            $this->genre = $genre;
+            $this->length = $length;
+        }
+
+        public function getMovieDetails()
+        {
+            $string = "Movie: ".$this->title.";<br/>Genre:".$this->genre.";<br/>Duration:"$this->length.";<br/>Year:"$this->year.";<br/>";
+
+            if(this->rating != NULL)
+                {
+                    $string.= "Rating: ".this->rating.";<br/>";
+                }
+
+                $genres_string = implode(',', $genres);
+                return $genres_string; 
+        }
+    }
 ?>
